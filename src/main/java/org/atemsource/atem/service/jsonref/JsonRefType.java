3c676b06b9924ddeafcc6a8a78a2eca0ef08b095
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-08-08 16:43:18 +0200

    moved code from example to here

---
+++ src/main/java/org/atemsource/atem/service/jsonref/JsonRefType.java
@@ -0,0 +1,19 @@
+package org.atemsource.atem.service.jsonref;
+
+import org.atemsource.atem.impl.common.attribute.primitive.PrimitiveTypeImpl;
+import org.codehaus.jackson.node.ObjectNode;
+
+public class JsonRefType extends  PrimitiveTypeImpl<ObjectNode> {
+
+	private static final String JSON_REF_TYPE_CODE = "json-ref";
+
+	
+	public String getCode() {
+		return JSON_REF_TYPE_CODE;
+	}
+
+	public Class<ObjectNode> getJavaType() {
+		return ObjectNode.class;
+	}
+
+}
