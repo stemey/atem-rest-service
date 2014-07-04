411957a771fc5e7f50a3a1a8937f68348a24ffbe
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-07-04 17:47:35 +0200

    removed old transformations

--- src/main/java/org/atemsource/atem/service/meta/service/binding/EditorConversion.java
+++ src/main/java/org/atemsource/atem/service/meta/service/binding/EditorConversion.java
@@ -2,10 +2,8 @@ package org.atemsource.atem.service.meta.service.binding;
 
 import org.atemsource.atem.api.type.EntityType;
 import org.atemsource.atem.service.gform.GformContext;
-import org.atemsource.atem.service.meta.service.binding.editor.EditorTransformationFactory;
 import org.atemsource.atem.utility.transform.api.JavaConverter;
 import org.atemsource.atem.utility.transform.api.TransformationContext;
-import org.atemsource.atem.utility.transform.impl.EntityTypeTransformation;
 import org.codehaus.jackson.node.ObjectNode;
 
 
